ae0f1bbc5629456ef1986f6c77e9c3a34a903f71
Huss3n <aisackhussein@gmail.com>
2024-03-19 11:45:26 +0300

    Kingfisher image loader

--- SwiftPackages/Packages/KingFisher/KingFisher.swift
+++ SwiftPackages/Packages/KingFisher/KingFisher.swift
@@ -6,10 +6,33 @@
 //
 
 import SwiftUI
+import Kingfisher
+
+
 
 struct KingFisher: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        KFImage(URL(string: "https://picsum.photos/seed/picsum/200/300"))
+            .placeholder { _ in
+                ProgressView()
+            }
+        
+        
+        
+        /*
+         // Use `KF` builder
+         KF.url(url)
+           .placeholder(placeholderImage)
+           .setProcessor(processor)
+           .loadDiskFileSynchronously()
+           .cacheMemoryOnly()
+           .fade(duration: 0.25)
+           .lowDataModeSource(.network(lowResolutionURL))
+           .onProgress { receivedSize, totalSize in  }
+           .onSuccess { result in  }
+           .onFailure { error in }
+           .set(to: imageView)
+         */
     }
 }
 
