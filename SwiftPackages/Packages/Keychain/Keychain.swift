9781a495dfd2506dc899c216c440f9e0580891a7
Huss3n <aisackhussein@gmail.com>
2024-03-19 12:13:58 +0300

    Keychain

---
+++ SwiftPackages/Packages/Keychain/Keychain.swift
@@ -0,0 +1,22 @@
+//
+//  Keychain.swift
+//  SwiftPackages
+//
+//  Created by Muktar Hussein on 19/03/2024.
+//
+
+import SwiftUI
+
+
+struct Keychain: View {
+    var body: some View {
+        Text("Hello world")
+    }
+}
+
+
+struct Keychain_Preview: PreviewProvider {
+    static var previews: some View {
+        Keychain()
+    }
+}
