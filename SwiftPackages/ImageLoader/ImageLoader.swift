0ca3c75b7e84e71f70dc3aca925765fba8b9e367
Huss3n <aisackhussein@gmail.com>
2024-03-19 11:24:45 +0300

    File restructure

---
+++ SwiftPackages/ImageLoader/ImageLoader.swift
@@ -0,0 +1,18 @@
+//
+//  ImageLoader.swift
+//  SwiftPackages
+//
+//  Created by Muktar Hussein on 19/03/2024.
+//
+
+import SwiftUI
+
+struct ImageLoader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageLoader()
+}
---
+++ SwiftPackages/Packages/KingFisher/KingFisher.swift
@@ -0,0 +1,18 @@
+//
+//  KingFisher.swift
+//  SwiftPackages
+//
+//  Created by Muktar Hussein on 19/03/2024.
+//
+
+import SwiftUI
+
+struct KingFisher: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KingFisher()
+}
