0b415e6e21f8bf2561fa6150586e738441f9e2d9
Huss3n <aisackhussein@gmail.com>
2024-03-19 12:14:44 +0300

    Keychain decription

--- SwiftPackages/Packages/Keychain/Keychain.swift
+++ SwiftPackages/Packages/Keychain/Keychain.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 
+// keychain is like user defaults and app storage
+// it is used to store and persist data between app sessions
+// For keychain the persisted data is encrypted
+// it is used to store sensitive information such as passwords, credit card details, pii
+// keychain persists between installs so if the apps deleted the data will remain in keychain  and used in the next install
+// keychain also persists data across devices 
+
 struct Keychain: View {
     var body: some View {
         Text("Hello world")
