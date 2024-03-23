c35df8a53c9fd4ba5a82492af7d51c34017b88cd
Huss3n <aisackhussein@gmail.com>
2024-03-23 11:14:54 +0300

    Lottie iOS

---
+++ SwiftPackages/LottieAnimation/LottieAnimation.swift
@@ -0,0 +1,18 @@
+//
+//  LottieAnimation.swift
+//  SwiftPackages
+//
+//  Created by Muktar Hussein on 23/03/2024.
+//
+
+import SwiftUI
+
+struct LottieAnimation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LottieAnimation()
+}
